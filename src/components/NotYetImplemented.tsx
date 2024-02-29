69c5fc189a6609fb846b95cea1412a881ce72512
Papsan <papsanlysenko@gmail.com>
2024-02-29 23:40:32 +0200

    change background on not yet implemented component

--- src/components/NotYetImplemented.tsx
+++ src/components/NotYetImplemented.tsx
@@ -2,12 +2,21 @@
 
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
 
 export default function NetYetImplemented() {
   const rounter = useRouter()
 
   return (
-    <div className={'bg-secondary p-4 rounded-sm space-y-1'}>
+    <div
+      className={cn(
+        'p-4',
+        'bg-[var(--main-page-background)]',
+        'rounded-sm',
+        'space-y-1',
+        'backdrop-blur-sm'
+      )}
+    >
       <h1 className={'font-bold text-xl'}>Not Yet Implemented</h1>
       <Button className={'w-full'} onClick={() => rounter.back()}>
         Back
